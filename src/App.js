c12ef6c281339aac6f2b51c7711189efd0ec9f24
aldopaganijr <aldojpagani@gmail.com>
2022-07-17 09:13:35 -0400

    images added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.scss"
 import Categories from "./components/categories/Categories"
+import ImageHeader from "./components/image-header/ImageHeader";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <ImageHeader />
       <Categories Categories={Categories} />
     </div>
   );
---
+++ src/components/image-header/ImageHeader.js
@@ -0,0 +1,12 @@
+import "./ImageHeader.scss"
+
+const imageHeader = () => {
+  return (
+
+    <div className="imageHeader">
+        <img src="https://images.unsplash.com/photo-1612817159949-195b6eb9e31a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"  alt="main-pic" />
+    </div>
+  )
+}
+
+export default imageHeader
