ba65502f44cd14defdb541064e18e5ac3eb97705
aldopaganijr <aldojpagani@gmail.com>
2022-07-22 23:46:13 -0400

    about section mobile done

--- src/components/routes/about/About.js
+++ src/components/routes/about/About.js
@@ -14,7 +14,7 @@ const About = () => {
           <div className="image-container">
             <img id="lady" src="https://images.unsplash.com/photo-1531058240690-006c446962d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="lady" />
             <div className="bio-container">
-            <h2 className="heading-two">The Grow Room</h2>
+            <h2 className="heading-grow-room">The Grow Room</h2>
             <hr />
               <p className="bio">The Grow Room was born from a deep-seated love and appreciation of the beauty in Mother Nature’s gift to us – flowers. Our dream is to make the experience of gifting and receiving blooms unique and magical for one and all.
             
